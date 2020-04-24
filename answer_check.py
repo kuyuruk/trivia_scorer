62a8fbd4abe8517538f4bf50fd89c1707007f356
kuyuruk <clausenkai2@gmail.com>
2020-04-23 21:57:03 -0700

    Update to use fuzz.WRatio() method and take threshold value from key on a per round basis.

--- answer_check.py
+++ answer_check.py
@@ -1,5 +1,4 @@
 from fuzzywuzzy import fuzz
-
 #-----------------------------------------------------------------------------#
 # Variables: 
 #   round:   string variable defining the trivia round
@@ -18,33 +17,41 @@ def check_round(round, answers, key, debug):
   
   # Determine Round
   if round == 'Round 1':
-    answer_round_str = 'round1_answers'
+    answer_round_str   = 'round1_answers'
+    fuzz_threshold_str = 'round1_threshold'
   elif round == 'Round 2':
-    answer_round_str = 'round2_answers'
+    answer_round_str   = 'round2_answers'
+    fuzz_threshold_str = 'round2_threshold'
   elif round == 'Round 3':
-    answer_round_str = 'round3_answers'
+    answer_round_str   = 'round3_answers'
+    fuzz_threshold_str = 'round3_threshold'
   elif round == 'Round 4':
-    answer_round_str = 'round4_answers'
+    answer_round_str   = 'round4_answers'
+    fuzz_threshold_str = 'round4_threshold'
   elif round == 'Bonus Round':
-    round_weight     = 5
-    answers          = answers[:1]
-    answer_round_str = 'bonus_answer'
-  
+    answer_round_str   = 'bonus_answer'
+    fuzz_threshold_str = 'bonus_threshold'
+    round_weight       = 5
+    answers            = answers[:1]
+
+  # Determine the fuzz_threshold for the round
+  fuzz_threshold = key['key'][fuzz_threshold_str]  
+
   # Do Answer Validation
   for i in range (0,len(answers)):
     key_values = key['key'][answer_round_str]
     if isinstance(key_values[i], list):
       for j in range(0,len(key_values[i])):
-        fuzz_val = fuzz.partial_ratio(answers[i], key_values[i][j])
+        fuzz_val = fuzz.WRatio(answers[i], key_values[i][j])
         if debug:
           print('key_val = ' + str(key_values[i][j]))
           print('answer = ' + str(answers[i]))
-          print('fuzz_val = ' + str(fuzz_val))
+          print(' fuzz_val = ' + str(fuzz_val))
         if(fuzz_val > fuzz_threshold):
           score += round_weight 
           break
     else:
-      fuzz_val = fuzz.partial_ratio(answers[i], key_values[i])
+      fuzz_val = fuzz.WRatio(answers[i], key_values[i])
       if debug:
         print('key_val = ' + str(key_values[i]))
         print('answer = ' + str(answers[i]))
--- scorer.py
+++ scorer.py
@@ -1,10 +1,9 @@
 # scorer.py - Calling syntax: python3 scorer.py starting_row ending_row -d
 #                             e.g. "python3 scorer.py 2 14" to parse rows 2 through 14
-#                             
-
+#
 # Import Libraries
 import gspread
-from   oauth2client.service_account import ServiceAccountCredentials
+from oauth2client.service_account import ServiceAccountCredentials
 import json
 import sys
 import answer_check as ac
@@ -16,48 +15,49 @@ print(sys.argv[0])
 debug = False
 
 if len(sys.argv) == 4:
-  if sys.argv[3] == '-d':
-    print(' Debug Mode\n')
-    debug = True
+    if sys.argv[3] == '-d':
+        print(' Debug Mode\n')
+        debug = True
 
 # Identify the Rows to be Analyzed (User Input)
 starting_row = int(sys.argv[1])
-ending_row   = int(sys.argv[2])
+ending_row = int(sys.argv[2])
 
 # Setup Google Dev API Access
 scope = ['https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('quarantrivia_client.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    'quarantrivia_client.json', scope)
 client = gspread.authorize(creds)
 
-# Open Sheet 
+# Open Sheet
 sheet = client.open("Week 3(Responses)").sheet1
 
 # Use JSON Answer Key for Answer Checking
 with open('week3_key.json') as week_key:
-  key_data = json.load(week_key)
+    key_data = json.load(week_key)
 
-  # Read Answers By Row
-  for row in range(starting_row, (ending_row + 1)):
-    row_entries = sheet.row_values(row)
-    team_name   = row_entries[1]
-    score       = 0
-    round       = row_entries[2]
-    answers     = row_entries[3:15]
+    # Read Answers By Row
+    for row in range(starting_row, (ending_row + 1)):
+        row_entries = sheet.row_values(row)
+        team_name = row_entries[1]
+        score = 0
+        round = row_entries[2]
+        answers = row_entries[3:15]
 
-    # Check Answers and Calculate Score
-    ret_val = ac.check_round(round, answers, key_data, debug)
-    if(ret_val < 0):
-      print("error scoring row: " + str(row))
-    else:
-      score = ret_val
-      sheet.update_cell(row, 16, score) # Write Score Into Spreadsheet
+        # Check Answers and Calculate Score
+        ret_val = ac.check_round(round, answers, key_data, debug)
+        if(ret_val < 0):
+            print("error scoring row: " + str(row))
+        else:
+            score = ret_val
+            sheet.update_cell(row, 16, score)  # Write Score Into Spreadsheet
 
-    # Print: team name, submitted answers, score by row
-    if(debug == True):
-      print('\n' + str(team_name))
-      print(answers)
-      print('row ' + str(row) + ' score = ' + str(score))
-      print('\n')
-    
+        # Print: team name, submitted answers, score by row
+        if(debug == True):
+            print('\n' + str(team_name))
+            print(answers)
+            print('row ' + str(row) + ' score = ' + str(score))
+            print('\n')
+
 # Print Exit Status
 print("Scoring complete for rows " + str(starting_row) + ":" + str(ending_row))
